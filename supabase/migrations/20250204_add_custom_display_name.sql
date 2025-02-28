d61529e9b55de3a56ab4441219038e475229f978
Fitcrates <aeonofshreds@gmail.com>
2025-02-28 15:58:30 +0100

    Delete supabase directory

--- supabase/migrations/20250204_add_custom_display_name.sql
+++
@@ -1,8 +0,0 @@
--- Add custom_display_name column to users table
-ALTER TABLE users ADD COLUMN IF NOT EXISTS custom_display_name TEXT;
-
--- Update RLS policies to allow users to update their own custom_display_name
-CREATE POLICY update_own_custom_display_name ON users
-    FOR UPDATE
-    USING (auth.uid() = id)
-    WITH CHECK (auth.uid() = id);
