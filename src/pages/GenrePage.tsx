9bbb946c01562e595746c92e6b24367eb4a56a10
Fitcrates <aeonofshreds@gmail.com>
2025-03-03 09:37:19 +0100

    Add files via upload

--- src/pages/GenrePage.tsx
+++ src/pages/GenrePage.tsx
@@ -385,7 +385,7 @@ const GenrePage: React.FC = () => {
 </div>
   
           {/* Results Section with card styling */}
-          <div className="max-w-[100rem] space-y-6 mx-auto px-0 sm:px-6 lg:px-8 mt-12">
+          <div className="max-w-[100rem] space-y-6 mx-auto px-1 sm:px-6 lg:px-8 mt-12">
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
               {animeList.map((anime, index) => (
                 <div key={`${anime.mal_id}-${index}`} className="relative group">
