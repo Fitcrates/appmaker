c1f85ae295de1f7cc2f71cb8587e102f5f48c6e7
Fitcrates <aeonofshreds@gmail.com>
2025-03-05 12:54:51 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ export function Footer() {
   }
 
   return (
-    <footer className="backgroundMain border-t border-white/40
+    <footer className="backgroundMain border-t-[#4ef1d6] drop-shadow-[0_0_8px_#4ef1d6]
  text-white/80 ">
       <div className="max-w-[100rem] space-y-6 mx-auto justify-center mt-12 px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ">
